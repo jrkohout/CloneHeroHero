3a58306d0269787ab8bf1fa45be9b6a6ad561e82
jrkohout <jrkohout64@gmail.com>
2022-03-02 23:25:21 -0600

    finished screencapture stuff including warp perspective and color selection.

--- main.py
+++ main.py
@@ -30,63 +30,6 @@ STRUM_DELAY_NS = 1 / STRUMS_PER_SECOND * 1e9
 
 AREA_THRESH = 100
 
-callback_img = None
-
-
-hsv_lowers = np.uint8([  # default
-    [55, 100, 100],   # green
-    [0, 100, 100],    # red
-    [25, 100, 100],   # yellow
-    [100, 100, 100],  # blue
-    [10, 100, 100]    # orange
-])
-hsv_uppers = np.uint8([  # default
-    [65, 255, 255],
-    [5, 255, 255],
-    [35, 255, 255],
-    [110, 255, 255],
-    [20, 255, 255]
-])
-hsv_setter_idx = 0
-
-
-def color_mouse_callback(event, x, y, flags, param):
-    global hsv_lowers
-    global hsv_uppers
-    global hsv_setter_idx
-    if event == cv2.EVENT_LBUTTONUP:
-        print("x: {}, y: {}".format(x, y))
-        # bgr - [0-255, 0-255, 0-255]
-        # hsv - [0-179, 0-255, 0-255]
-        bgr = callback_img[y, x, :]
-        hsv = cv2.cvtColor(callback_img[y, x, :].reshape((1, 1, 3)), cv2.COLOR_BGR2HSV)
-        print("BGR:", bgr)
-        print("HSV:", hsv)
-        if hsv_setter_idx < 5:
-            hsv_lowers[hsv_setter_idx] = np.uint8([max(hsv[0, 0, 0] - 5, 0), 100, 100])  # TODO - adjust blue maybe
-            hsv_uppers[hsv_setter_idx] = np.uint8([min(hsv[0, 0, 0] + 5, 179), 255, 255])
-            print("set color ranges for", hsv_setter_idx)
-            hsv_setter_idx += 1
-
-
-
-def divide_boundbox(mss_base):
-    global callback_img
-
-    # set colors
-    callback_img = cv2.imread("screenshots/boundbox_example.png") # fixme - somehow get screenshot of actual fretboard
-    cv2.imshow("screenshot", callback_img)
-    cv2.setMouseCallback("screenshot", color_mouse_callback)
-
-    hsv_img = cv2.cvtColor(callback_img, cv2.COLOR_BGR2HSV)
-    _ = cv2.waitKey()
-
-    for i in range(5):
-        mask = cv2.inRange(hsv_img, hsv_lowers[i], hsv_uppers[i])  # red mask
-        cv2.imshow("screenshot", mask)
-        _ = cv2.waitKey()
-
-
 strum_counter = 0
 
 
