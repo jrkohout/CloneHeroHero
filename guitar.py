c63a9141a9eb3ed6aeaf81988be85306fce063a6
jrkohout <jrkohout64@gmail.com>
2022-03-04 12:05:54 -0600

    implemented a parallel thread that constanly dequeues notes and busy-waits until the specified delay has passed before strumming. This seems to have mitigated a lot of the delay issues from before. The next work will involve better color segmentation, and probably reducing the amount of board sent over by screencapture. Should also optimize more, and possibly try to deal with held notes (notes with a long tail where you hold down the key - these group up as another note according to the connected components algorithm)

--- guitar.py
+++ guitar.py
@@ -2,10 +2,11 @@
 
 import time
 import numpy as np
-from collections import deque
-from pyKey import pressKey, releaseKey, press, sendSequence, showKeys
+from queue import SimpleQueue
+from pyKey import pressKey, releaseKey, press
+from threading import Thread
 
-STRUM_DELAY_MS = 0
+STRUM_DELAY_MS = 10
 STRUM_DELAY_NS = STRUM_DELAY_MS * 1e6
 
 # in-game keybinds for green, red, yellow, blue, and orange, respectively
@@ -18,20 +19,35 @@ STRUM_KEY = 'DOWN'
 class Guitar:
     def __init__(self):
         # use .appendleft() to push, .pop() to pop, and [-1] to peek
-        self._strum_queue = deque()
+        self._strum_queue = SimpleQueue()
+        self._thread = Thread(target=self.check_strum)
 
     # only push a note mask if at least one note is to be strummed
-    def push_strum(self, note_list):
-        self._strum_queue.appendleft((time.perf_counter_ns(), note_list))
+    def enqueue_strum(self, note_list):
+        self._strum_queue.put((True, time.perf_counter_ns(), note_list))  # True - continue
 
-    def check_strum(self):  # TODO - this could potentially be run constantly on another thread (using proper synchronization constructs)
-        if len(self._strum_queue) > 0:
-            call_time = self._strum_queue[-1][0]
-            if time.perf_counter_ns() - call_time >= STRUM_DELAY_NS:
-                _, note_mask = self._strum_queue.pop()
+    def check_strum(self):
+        keep_looping = True
+        while keep_looping:
+            keep_looping, call_time, note_mask = self._strum_queue.get()
+            if keep_looping:
+                # wait until the delay has passed
+                while time.perf_counter_ns() - call_time < STRUM_DELAY_NS:
+                    pass
+                # strum the notes
                 notes = NOTE_KEYS[note_mask]
                 for note in notes:
                     pressKey(note)
                 press(STRUM_KEY)
                 for note in notes:
                     releaseKey(note)
+        self._thread.join()
+
+    # start the guitar's check_strum loop
+    def start_thread(self):
+        self._thread.start()
+        print("Started guitar thread")
+
+    # enqueues the end-signal to have check_strum() terminate and join
+    def end_thread(self):
+        self._strum_queue.put((False, 0, np.zeros(5).astype(bool)))
