27e98ac0f9ea1ad7b402a961e3a8c4b512b075d0
jrkohout <jrkohout64@gmail.com>
2022-03-06 00:22:17 -0600

    Completed a pretty darn good version of the bot. Using a Ryzen 2600 CPU, got 99% accuracy on Through The Fire and Flames on Expert mode. Honestly very surprised at that. CPU usage also doesn't really go above like 15%, so I think the bot is decently optimized, but there are a few things that could still be improved. I included many TODO statements for things to explore moving forward with this version of the bot. It is hard to tell what the bot messes up on, but there appears to be a pattern to it since all the mistakes on the aforementioned Dragonforce song happened in one section. I think it would be best to explore these errors by screen recording both the bot and the Clone Hero game playing and analyzing the frames surrounding the mistakes. Could also just add another feed view to the bot to avoid recording both screens if that's not possible. Other than that, I am very satisfied with the current state of the bot, and there is the potential for 100% accuracy. I also want to add support for holding long notes (note tails) and open strums (purple empty strums) before calling the bot complete. Maybe also whammy support. Also those stupid half-transparent notes in some of the new charts. And.....

--- guitar.py
+++ guitar.py
@@ -15,12 +15,18 @@ class Guitar:
     # only push a note mask if at least one note is to be strummed
     def enqueue_strum(self, note_mask):
         # items: (call_time, note_mask)
-        self._strum_queue.appendleft((time.perf_counter_ns(), note_mask))
+        self._strum_queue.appendleft((time.perf_counter_ns(), note_mask))  # TODO - could use better optimized queue
 
     def check_strum(self):
         # only strum if the delay has passed
-        if len(self._strum_queue) > 0 and time.perf_counter_ns() - self._strum_queue[-1][0] >= settings.STRUM_DELAY_NS:
+        if len(self._strum_queue) > 0 and time.perf_counter_ns() - self._strum_queue[-1][0] >= settings.STRUM_DELAY_NS:  # TODO - could use better checks
             _, note_mask = self._strum_queue.pop()
+
+            # TODO - could MAYBE have a thread block for a signal on get(), and only send a signal to play the notes
+            #  when the delay has passed in the many check_strum calls currently in main. This would prevent the main
+            #  thread from having to strum the notes, which might could possibly take some time. Though with the other
+            #  threading experiments, this seems unlikely to help (but I haven't tried on a powerful PC).
+
             # strum the notes
             notes = settings.NOTE_KEYS[note_mask]
             for note in notes:
